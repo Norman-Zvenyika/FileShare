74a54e8a27e659a17237c6dc78cb98b5176de06c
noz224 <noz224@lehigh.edu>
2022-08-12 17:10:55 -0400

    QUESTION1-1

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -5,6 +5,11 @@
 #include <vector>
 #include "pack109.hpp"
 #include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <algorithm>
+#include <cctype>
+#include <fstream>
 
 using namespace pack109;
 using namespace std;
@@ -57,7 +62,42 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
+    //https://www.tutorialspoint.com/unix_sockets/socket_quick_guide.htm
     //create sockets
+    int sockfd, newsockfd, portno;
+    socklen_t  clilen;
+    char buffer[10000];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    
+    /* First call to socket() function */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+    
+    /* Initialize socket structure */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = stoi(portString);
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    
+    /* Now bind the host address using bind() call.*/
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+        
+    /* Now start listening for the clients, here process will
+        * go in sleep mode and will wait for the incoming connection
+    */
+    
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
 
     //store the bytes received
 

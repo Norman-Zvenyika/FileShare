c2e1899548bbb8a6e70c60e2698fa89fea38bd9a
noz224 <noz224@lehigh.edu>
2022-08-12 16:36:24 -0400

    add plan outline

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -5,3 +5,60 @@
 #include <vector>
 #include "pack109.hpp"
 #include <unistd.h>
+
+using namespace pack109;
+using namespace std;
+
+int main(int argc, char** argv) {
+    //declare variables
+
+    //terminate if less than 2 arguments are given
+
+    //parse the input arguments
+
+    //check if host name is present
+
+    //create sockets
+
+    //store the bytes received
+
+    //determine if it is a request/file
+
+        //request
+
+        //decrypt the request
+
+        //deserialize the request
+
+        //get the name of the file
+
+        //check if file exists
+
+        //if file exists
+
+            //open file and read in memory
+
+            //create a struct and store the bytes
+
+            //serialize the sendfile vec
+
+            //encrypt the serializedSendFile vec
+
+            //then send file to server
+
+        //doesn't exist
+
+            //notify the client
+    
+    //if it's the file
+
+        //store the file
+
+        //decrypt file contents
+
+        //deserialize the decrypted vector
+
+        //write to a file and store it in the received folder
+
+    return 0;
+}
